9b64686f979a296032f8d572414c049f52246599
Pierre Miniggio <pierre.miniggio@gmail.com>
2020-12-30 00:03:24 +0100

    use config instead

--- config_example.php
+++ config_example.php
@@ -1,18 +1,15 @@
 <?php
 
 return [
+    'youtube_api' => [
+        'client_id' => '',
+        'client_secret' => '',
+        'refresh_token' => ''
+    ],
     'db' => [
         'host' => '',
         'database' => 'channel-storage',
         'username' => '',
         'password' => ''
-    ],
-    'groups' => [
-        [
-            'youtube' => 'catoonthecat'
-        ],
-        [
-            'youtube' => 'pierremorph'
-        ]
     ]
 ];
--- src/App.php
+++ src/App.php
@@ -4,7 +4,7 @@ namespace PierreMiniggio\YoutubeChannelVideoInformationsSaver;
 
 use PierreMiniggio\YoutubeChannelVideoInformationsSaver\Connection\DatabaseConnectionFactory;
 use PierreMiniggio\YoutubeChannelVideoInformationsSaver\Repository\YoutubeVideoRepository;
-use PierreMiniggio\YoutubeChannelVideoInformationsSaver\Youtube\LatestVideosFetcher as LatestYoutubeVideoFetcher;
+use PierreMiniggio\YoutubeChannelVideoInformationsSaver\Youtube\YoutubeVideo;
 
 class App
 {
@@ -12,27 +12,108 @@ class App
     {
         $config = require(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'config.php');
 
-        $lastestYoutubeVideosFetcher = new LatestYoutubeVideoFetcher();
+        $code = 0;
 
-        if ($hasDB = ! empty($config['db'])) {
-            $repository = new YoutubeVideoRepository((new DatabaseConnectionFactory())->makeFromConfig($config['db']));
+        if (empty($config['db'])) {
+            echo 'Missing DB config';
+
+            return $code;
         }
-    
-        foreach ($config['groups'] as $group) {
 
-            $youtubeChannel = $group['youtube'];
-            echo PHP_EOL . PHP_EOL . 'Channel : ' . $youtubeChannel;
+        if (empty($config['youtube_api'])) {
+            echo 'Missing Youtube API config';
 
-            $youtubeVideos = array_reverse($lastestYoutubeVideosFetcher->fetch($youtubeChannel));
+            return $code;
+        }
 
-            foreach ($youtubeVideos as $youtubeVideo) {
-                if ($hasDB) {
-                    echo PHP_EOL . 'Inserting/updating ' . $youtubeVideo->getId() . ' from channel ' . $youtubeVideo->getChannel() . ' ...';
-                    $repository->addIfMissing($youtubeVideo);
-                    echo PHP_EOL . $youtubeVideo->getId() . ' inserted/updated !';
-                }
+        $repository = new YoutubeVideoRepository((new DatabaseConnectionFactory())->makeFromConfig($config['db']));
+
+        //////
+        $accessTokenCurl = curl_init();
+        curl_setopt_array($accessTokenCurl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://www.googleapis.com/oauth2/v4/token',
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => http_build_query(array_merge([
+                'grant_type' => 'refresh_token'
+            ], $config['youtube_api']))
+        ]);
+        $accessTokenCurlResult = curl_exec($accessTokenCurl);
+
+        if ($accessTokenCurlResult === false) {
+            echo 'Error curl';
+
+            return $code;
+        }
+
+        $accessTokenJsonResponse = json_decode($accessTokenCurlResult);
+        if (! empty($accessTokenJsonResponse->error)) {
+            echo 'Error ' . $accessTokenJsonResponse->error->code . ': ' . $accessTokenJsonResponse->error->message;
+
+            return $code;
+        }
+
+        $channelId = 'UCKbunxMnqxM8UFMEYF79CUQ';
+        echo PHP_EOL . PHP_EOL . 'Channel : ' . $channelId;
+
+        $channelVideosCurl = curl_init();
+        curl_setopt_array($channelVideosCurl, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => 'https://www.googleapis.com/youtube/v3/search?channelId=' . $channelId . '&part=id&order=date&maxResults=20'
+        ]);
+        $authorization = "Authorization: Bearer " . $accessTokenJsonResponse->access_token;
+        curl_setopt($channelVideosCurl, CURLOPT_HTTPHEADER, ['Content-Type: application/json' , $authorization]);
+
+        $channelVideosCurlResult = curl_exec($channelVideosCurl);
+        $channelVideosJsonResponse = json_decode($channelVideosCurlResult, true);
+
+        if (! empty($channelVideosJsonResponse['error'])) {
+            echo 'Error ' . $channelVideosJsonResponse['error']['code'] . ': ' . $channelVideosJsonResponse['error']['message'];
+
+            return $code;
+        }
+
+        $videoIds = array_map(
+            fn ($channelVideoJsonResponse) => $channelVideoJsonResponse['id']['videoId'],
+            $channelVideosJsonResponse['items']
+        );
+        
+        foreach ($videoIds as $videoId) {
+
+            echo PHP_EOL . 'Inserting/updating ' . $videoId . ' from channel ' . $channelId . ' ...';
+
+            $videoCurl = curl_init();
+            curl_setopt_array($videoCurl, [
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => 'https://www.googleapis.com/youtube/v3/videos?id=' . $videoId . '&part=snippet'
+            ]);
+            $authorization = "Authorization: Bearer " . $accessTokenJsonResponse->access_token;
+            curl_setopt($videoCurl, CURLOPT_HTTPHEADER, ['Content-Type: application/json' , $authorization]);
+
+            $videoCurlResult = curl_exec($videoCurl);
+
+            $videoJsonResponse = json_decode($videoCurlResult, true);
+
+            if (! empty($videoJsonResponse['error'])) {
+                echo 'Error ' . $videoJsonResponse['error']['code'] . ': ' . $videoJsonResponse['error']['message'];
+
+                return $code;
             }
+
+            $snippet = $videoJsonResponse['items'][0]['snippet'];
+            $youtubeVideo = new YoutubeVideo(
+                $channelId,
+                $videoId,
+                'https://www.youtube.com/watch?v=' . $videoId,
+                $snippet['thumbnails']['high']['url'],
+                $snippet['title'],
+                $snippet['description']
+            );
+            $repository->addIfMissing($youtubeVideo);
+            echo PHP_EOL . $youtubeVideo->getId() . ' inserted/updated !';
         }
+
+        /////
 
         echo PHP_EOL . PHP_EOL . 'Done !';
 
